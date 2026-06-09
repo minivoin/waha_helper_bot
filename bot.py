7cec3f659cc04fe1419c6cfce396ad4d0ee1f0b1
minivoin <bonycat759@gmail.com>
2026-06-09 18:17:24 +0700

    ver 0.2.2

--- bot.py
+++ bot.py
@@ -1,18 +1,40 @@
-from aiogram import Bot, Dispatcher# type: ignore
-from handlers import router
 import asyncio
+import os 
+from aiogram import Bot, Dispatcher  # type: ignore
+from handlers import router
+from aiohttp import web  
+
+PORT = int(os.getenv("PORT", 8080))
+
+
+async def handle_ping(request):
+    return web.Response(text="Бот работает!", status=200)
+
+
+async def start_web_server():
+    app = web.Application()
+    app.router.add_get("/", handle_ping)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", PORT)
+    await site.start()
 
 
 async def main():
-    bot = Bot(token='Сюда токен бота') ## ТОКИН БОТА
+    bot = Bot(token=os.getenv("BOT_TOKEN", "Сюда токен бота"))
     dp = Dispatcher()
+
     dp.include_router(router)
+
+    asyncio.create_task(start_web_server())
+
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     try:
-        print('БОТ РАБОТАЕТ')
+        print("БОТ РАБОТАЕТ")
         asyncio.run(main())
-    except KeyboardInterrupt():
-        print('БОТ ВЫКЛЮЧЕН')    
-
+    except KeyboardInterrupt: 
+        print("БОТ ВЫКЛЮЧЕН")
